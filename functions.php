66cad54fedf36dabcbc6c68d1f1b6d8fd96bbc0b
shinzoku <shinzoku62800@gmail.com>
2022-06-22 16:43:20 +0200

    correction d'un commentaire

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function my_theme_enqueue_styles() {
     // chargement de la feuille de style du thème parent
     wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
     // chargement de la feuille de style du thème enfant
-    // wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', ['untitled-style']); // si la feuille parent prend le dessus on supprime la première ligne et active ceci.
+    // wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', ['untitled-style']); // si la feuille parent prend le dessus on supprime la ligne du dessous et active ceci.
     wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', ['parent-style','untitled-webfont','untitled-flex-slider-style']);
 }
 
